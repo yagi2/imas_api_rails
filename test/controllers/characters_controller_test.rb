56d92f348f2e5090ef1eb77d11a5ac78d61a6431
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-12-15 22:02:44 +0900

    Remove unneed controller tests

--- test/controllers/characters_controller_test.rb
+++ test/controllers/characters_controller_test.rb
@@ -9,30 +9,4 @@ class CharactersControllerTest < ActionDispatch::IntegrationTest
     get characters_url, as: :json
     assert_response :success
   end
-
-  test "should create character" do
-    assert_difference('Character.count') do
-      post characters_url, params: { character: {  } }, as: :json
-    end
-
-    assert_response 201
-  end
-
-  test "should show character" do
-    get character_url(@character), as: :json
-    assert_response :success
-  end
-
-  test "should update character" do
-    patch character_url(@character), params: { character: {  } }, as: :json
-    assert_response 200
-  end
-
-  test "should destroy character" do
-    assert_difference('Character.count', -1) do
-      delete character_url(@character), as: :json
-    end
-
-    assert_response 204
-  end
 end
