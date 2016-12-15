bb5050466a1213abe8d7227fd8a4e14dc5c34e66
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-12-15 22:29:08 +0900

    Check response body

--- test/controllers/characters_controller_test.rb
+++ test/controllers/characters_controller_test.rb
@@ -8,5 +8,6 @@ class CharactersControllerTest < ActionDispatch::IntegrationTest
   test "should get all" do
     get characters_all_url, as: :json
     assert_response :success
+    assert_match /天海春香/, @response.body
   end
 end
